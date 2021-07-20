859f75c2be1f2fe9a049b67b409d1280ec423c77
Mason Mackaman <masondeanm@aol.com>
2021-07-20 18:28:55 -0400

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ pkgs
+  ? import
+      (fetchTarball
+         { url = "https://github.com/NixOS/nixpkgs/archive/c7e7f90108ff7bb7924e6f70136dd72c0f916954.tar.gz";
+           sha256 = "0k7y9zx37jsbqf2jh8gk6l4q8qv19lpnyqhigj54llz9s5c4zszp";
+         }
+      )
+      {}
+, module
+}:
+  (pkgs.lib.evalModules
+     { modules =
+         [ { _module.args = { inherit pkgs; }; }
+           ./module.nix
+           module
+         ];
+     }
+  ).config
