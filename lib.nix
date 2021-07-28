0e16526e204c4ddb13927a67fb88e8df3852efa6
Mason Mackaman <masondeanm@aol.com>
2021-07-28 04:23:22 -0400

    `merge` no longer accepts attribute sets with `extra-rules`
    This is a footgun, as it allows you to give extra rules to one class and
    unknowingly have them generated with a another class's selector.
    
    A new function has been introduced that keeps `merge`'s old
    functionality.

--- lib.nix
+++ lib.nix
@@ -1,5 +1,17 @@
 with builtins;
 lib:
+  let
+    l = lib;
+
+    merge-with-check = check: error-msg: classes:
+      if check classes then
+        (import ./types.nix lib).class-type.merge []
+          (map (c: { value = c; file = "unknown"; }) classes)
+      else
+        abort error-msg;
+
+    merge-allowing-extra-rules-name = "merge-all";
+  in
   { media =
       rec
       { between = lower: upper: inclusive-between (lower + 1) upper;
@@ -16,7 +28,19 @@ lib:
 
     make-var-values = mapAttrs (n: _: "var(--${n})");
 
-    merge = classes:
-      (import ./types.nix lib).class-type.merge []
-        (map (c: { value = c; file = "unknown"; }) classes);
+    merge =
+      merge-with-check
+        (all (a: !a?extra-rules))
+        ''
+        You are trying to merge classes that define `extra-rules`. This is not allowed by default, as it can easily have unintended side effects if you're not careful. To get around this, you can use the function `css-lib.${merge-allowing-extra-rules-name}`. However, if you only need to merge classes without `extra-rules` into an attribute set literal that has `extra-rules`, it is recommended that you do the following:
+
+            merge
+              [ class1
+                class2
+                { ... }
+              ]
+            // { extra-rules = c: { ... }; }
+        '';
+
+    ${merge-allowing-extra-rules-name} = merge-with-check (l.const true) "";
   }
