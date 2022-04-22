0e73df170a6686fd0c46bebfc00be2113e930dd7
Mason Mackaman <masondeanm@aol.com>
2022-04-22 15:16:03 -0400

    save

--- docs.nix
+++ docs.nix
@@ -38,70 +38,80 @@ with builtins;
       f [] l.id (a2l attrs);
 
     md =
+      let
+        functions = recurse-with-name library;
+
+        page-link = name:
+          h.a
+            { href = "#${name}";
+
+              style =
+                { text-decoration = "none";
+                  color = "inherit";
+                };
+            }
+            name;
+      in
       p.writeText "docs.md"
         (h.element "html" ""
-           [ (h.head ""
-                (h.style ""
-                   ''
-                   td {
-                     border: 1px solid black;
-                     padding: 5px;
-                   }
-
-                   pre {
-                     white-space: pre-wrap;
-                   }
-                   ''))
-
+           [ (h.head "" "")
              (h.body
                 { style =
                     { font = "1rem sans-serif";
+                      margin = 0;
+                      display = "grid";
+                      grid-template-columns = "max-content 1fr";
+                      # height = "100%";
                     };
                 }
-                (l.concatStringsSep (h.hr "")
-                   (map
-                      ({ name, value }:
-                         h.div ""
-                           [ (h.h2
-                                { id = name; }
-                                (h.a
-                                   { href = "#${name}";
+                [ (h.div ""
+                     (map
+                        ({ name, ... }: h.div "" (page-link name))
+                        functions))
 
-                                     style =
-                                       { text-decoration: "none";
-                                         color: "inherit";
-                                       }
-                                   }
-                                   name))
+                  (h.div
+                     { style =
+                         { height = "100vh";
+                           overflow-y = "scroll";
+                         };
+                     }
+                     (l.concatStringsSep (h.hr "")
+                        (map
+                           ({ name, value }:
+                              h.div ""
+                                [ (h.h2 { id = name; } (page-link name))
 
-                             (h.table { style = "border-collapse: collapse;"; }
-                                (map
-                                   ({ name, description }:
-                                      let
-                                        td =
-                                          h.td
-                                            { style =
-                                                { border = "1px solid black";
+                                  (h.table { style.border-collapse = "collapse"; }
+                                     (map
+                                        ({ name, description }:
+                                           let
+                                             td =
+                                               h.td
+                                                 { style =
+                                                     { border = "1px solid black";
+                                                       padding = "5px";
+                                                     };
+                                                 };
+                                           in
+                                           h.tr ""
+                                             [ (td (h.code "" name))
+                                               (td description)
+                                             ])
+                                        value.args))
 
-                                      in
-                                      h.tr ""
-                                        [ (h.td "" (h.code "" name))
-                                          (h.td "" description)
-                                        ])
-                                   value.args))
+                                  (if value?notes
+                                   then h.p { style.white-space = "pre-wrap"; } value.notes
+                                   else ""
+                                  )
 
-                             (if value?notes
-                              then h.p "pre" value.notes
-                              else ""
-                             )
-
-                             (h.h3 "" "Returns")
-                             (h.div "" value.returns)
-                             (h.h3 "" "Examples")
-                             (map (e: h.div "" (h.code "" e)) value.examples)
-                           ]
-                      )
-                      (recurse-with-name library))))
+                                  (h.h3 "" "Returns")
+                                  (h.div "" value.returns)
+                                  (h.h3 "" "Examples")
+                                  (map (e: h.div "" (h.code "" e)) value.examples)
+                                ]
+                           )
+                           functions)))
+                ])
            ]);
 
     markdown = str:
